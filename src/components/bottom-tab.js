7efd9db96af7781eb3d262fa9586aa585c288c1f
trdp30 <trdp30@gmail.com>
2020-11-11 23:21:58 +0530

    bottom tab added mobile view

--- src/components/bottom-tab.js
+++ src/components/bottom-tab.js
@@ -1,29 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
 
 function BottomTab() {
+  const [activeItem, setActiveItem] = useState("");
+
+  const handleItemClick = (e, { name }) => setActiveItem(name);
   return (
-    <div className="bottom-tab">
-      <div className="ui fluid three item labeled icon menu">
-        <Link to="/admin/dashboard" className="item padding-vs-top padding-vs-bottom">
-          <i aria-hidden="true" className="icon">
-            <img src="/assets/icons/desktop.png" alt="" className="bottom-tab-icon" />
-          </i>
-          <div className="bottom-tab-item">DASHBOARD</div>
-        </Link>
-        <Link to="/admin/stocks" className="item padding-vs-top padding-vs-bottom">
-          <i aria-hidden="true" className="icon">
-            <img src="/assets/icons/home.png" alt="" className="bottom-tab-icon" />
-          </i>
-          <div className="text-center bottom-tab-item">STOCK</div>
-        </Link>
-        <Link to="/admin/stocks" className="item padding-vs-top padding-vs-bottom">
-          <i aria-hidden="true" className="icon">
-            <img src="/assets/icons/list.png" alt="" className="bottom-tab-icon" />
-          </i>
-          <div className="text-center bottom-tab-item">ORDERS</div>
-        </Link>
-      </div>
+    <div className="bottom-tab mobile-menu">
+      <Menu>
+        <Menu.Item as="div">
+          <Link to="/">H</Link>
+        </Menu.Item>
+        <Menu.Menu position="right">
+          <Menu.Item
+            as="div"
+            name="category"
+            active={activeItem === "category"}
+            onClick={handleItemClick}
+          >
+            <Link to="/category">Category</Link>
+          </Menu.Item>
+
+          <Menu.Item as="div" name="shop" active={activeItem === "shop"} onClick={handleItemClick}>
+            <Link to="/shop">Shop</Link>
+          </Menu.Item>
+
+          <Menu.Item
+            as="div"
+            name="sign-in"
+            active={activeItem === "sign-in"}
+            onClick={handleItemClick}
+          >
+            <Link to="/">Sign In</Link>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
     </div>
   );
 }
--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+import BottomTab from "./bottom-tab";
 
 function NavBar() {
+  const [activeItem, setActiveItem] = useState("");
+
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
   return (
     <>
-      <div className="ui stackable top fixed menu">
-        <Link to="/" className="item">
-          <div className="text-center bottom-tab-item">H</div>
-        </Link>
-        <div className="right menu">
-          <Link to="/category" className="item">
-            <div className="text-center bottom-tab-item">Categories</div>
-          </Link>
-          <Link to="/shop" className="item">
-            <div className="text-center bottom-tab-item">Shops</div>
-          </Link>
-          <div className="item">Sign-in</div>
-        </div>
-      </div>
+      <Menu stackable className="desktop-menu">
+        <Menu.Item as="div">
+          <Link to="/">H</Link>
+        </Menu.Item>
+        <Menu.Menu position="right">
+          <Menu.Item
+            as="div"
+            name="category"
+            active={activeItem === "category"}
+            onClick={handleItemClick}
+          >
+            <Link to="/category">Category</Link>
+          </Menu.Item>
+
+          <Menu.Item as="div" name="shop" active={activeItem === "shop"} onClick={handleItemClick}>
+            <Link to="/shop">Shop</Link>
+          </Menu.Item>
+
+          <Menu.Item
+            as="div"
+            name="sign-in"
+            active={activeItem === "sign-in"}
+            onClick={handleItemClick}
+          >
+            <Link to="/">Sign In</Link>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+      <BottomTab />
     </>
   );
 }
